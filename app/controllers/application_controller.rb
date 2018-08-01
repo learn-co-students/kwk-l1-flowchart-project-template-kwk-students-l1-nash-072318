f475226a55252fa1a7929c9b270119f7cba10110
christina15339 <clamberg333@gmail.com>
2018-08-01 20:42:53 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, 'app/views' 
+  end 
+  
+    get '/charity_quiz' do
+    erb :charity_quiz
   end
 
+post '/charity' do
+  @name = params[:username]
+  @name = params[:question]
+  "A charity that best suits your preferences is #{charity}"
+  end
+  
 end
