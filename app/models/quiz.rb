14da61c04f200b35ce7a33da114b686b2a95d3fc
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-08-01 16:58:56 +0000

    Done.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,28 @@
 class Quiz
 
+attr_accessor :name, :age, :breed 
+
+@alldogs = []
+
+def initialize(name, q1, q2, q3)
+  @name = name
+  @q1 = q1
+  @q2 = q2
+  @q3 = q3
+
+end
+
+def results
+  if @q1 + @q2 + @q3 < 5
+    @breed = "golden retriever"
+  elsif @q1 + @q2 + @q3 < 8
+    @breed = "german shepard"
+  elsif @q1 + @q2 + @q3 < 11
+    @breed = "terrier"
+  else @q1 + @q2 + @q3 < 13
+    @breed = "pug"
+  end
+  @breed
+end  
+
 end
