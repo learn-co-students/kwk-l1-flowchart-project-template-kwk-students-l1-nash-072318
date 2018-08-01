0a07e539a2ef6a49123d11979076dd1879399cc6
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-08-01 18:54:12 +0000

    Done.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,11 +6,15 @@ attr_accessor :name, :age, :breed
 
 @alldogs = []
 
-def initialize(name, q1, q2, q3)
+def initialize(name, q1, q2, q3, q4, q5, q6, q7)
   @name = name
   @q1 = q1
   @q2 = q2
   @q3 = q3
+  @q4 = q4
+  @q5 = q5
+  @q6 = q6
+  @q7 = q7
 
 end
 
