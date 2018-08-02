9d4eaf808f37c110fb0139bde286c5e328ebd85a
elizabeth1018 <lizz_101@icloud.com>
2018-08-02 19:13:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,61 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+configure do
+ set :public_folder, 'public'
+ set :views, 'app/views'
+end
+
+get '/' do
+ erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/kode' do
+  erb :kode
+end
+ 
+get '/quizzz' do
+  erb :quizzz
+end
+
+get '/index' do
+  erb :index
+end
+
+# get '/result' do
+#   erb :result.rb
+#   end
+
+post '/results' do
+ @length = params[:length].to_i
+ @topic = params[:topic].to_i
+ @username = params[:username]
+# @response = Result.new(params[:username], params[:length].to_i, params[:topic].to_i)
+# @response.results
+# "Your article is #{@response.results}."
+ if @length + @topic <= 4
+erb :short_envior
+elsif @length + @topic <= 7
+erb :med_envior
+elsif @length + @topic <= 10
+ erb :short_feelgood
+elsif @length + @topic <= 12
+erb :short_womenintech
+elsif @length + @topic <= 14
+erb :med_feelgood
+elsif @length + @topic <= 15
+erb :long_envior
+elsif @length + @topic <= 16
+erb :med_womenintech
+elsif @length + @topic <= 22
+erb :long_feelgood
+else @length + @topic <= 24
+erb :long_womenintech
+end
 
 end
+end
+  
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,37 @@
 class Result
 
+attr_accessor :username, :length, :topic
+
+@allarticles = []
+
+def initialize(name, length, topic)
+@name = name
+@length = length
+@topic = topic
+end
+
+def results
+if @length + @topic <= 4
+  @article = "short_enviorment"
+elsif @length + @topic <= 7
+  @article = "med_enviorment"
+elsif @length + @topic <= 10
+   @article = "feelgood_short"
+ elsif @length + @topic <= 12
+  @article = "short_womenintech"
+  elsif @length + @topic <= 14
+  @article = "feelgood_med"
+  elsif @length + @topic <= 15
+  @article = "long_enviorment"
+  elsif @length + @topic <= 16
+  @article = "med_womenintech"
+  elsif @length + @topic <= 22
+  @article = "long_feelgood"
+  else @length + @topic <= 24
+    @article = "long_womenintech"
+ 
+ end
+@article
+end
+
 end
