b4f60a1f3e4e6ab5ae15144a43b90a715d571162
christina15339 <clamberg333@gmail.com>
2018-08-03 15:55:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,28 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views' 
   end 
   
-    get '/charity_quiz' do
+    get '/' do
     erb :charity_quiz
   end
 
+get '/animal' do 
+  erb :animal 
+end 
+
+
 post '/charity' do
   @name = params[:username]
-  @name = params[:question]
-  #@name = params[:question2]
-  #@name = params[:question3]
+  @answer = params[:q1]
+
+ if @answer.to_i == 2
+    erb :animal
+  else
+    erb :human
+ end
+end 
   
-  end
-  
+get '/charity' do
+erb :charity
 end
+ 
+end 
